95a0afd6219550bd21331911906d4df8c69b1b88
r1nzl3rrr <khanhle9344@gmail.com>
2025-01-27 22:08:11 +0700

    change user required property

--- Teamo.Core/Entities/Identity/User.cs
+++ Teamo.Core/Entities/Identity/User.cs
@@ -6,16 +6,16 @@ namespace Teamo.Core.Entities.Identity
 {
     public class User : IdentityUser<int>
     {
-        public required string Code { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required Gender Gender { get; set; }
+        public string Code { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
         public DateOnly Dob { get; set; }
         public string Phone { get; set; }
         public string ImgUrl { get; set; }
-        public required UserStatus Status { get; set; }
+        public UserStatus Status { get; set; }
         public string Description { get; set; }
-        public required int MajorID { get; set; }
+        public int MajorID { get; set; }
         public Major Major {  get; set; }
         public List<Link> Links { get; set; }
         public List<Skill> Skills { get; set; }
