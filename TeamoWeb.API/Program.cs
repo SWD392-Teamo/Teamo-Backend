6b2561ea4a9f353200a3a816f27f4e8e0d44b362
r1nzl3rrr <khanhle9344@gmail.com>
2025-01-28 16:51:16 +0700

    small changes to startup program

--- TeamoWeb.API/Program.cs
+++ TeamoWeb.API/Program.cs
@@ -15,13 +15,6 @@ builder.Services.AddSingleton(TimeProvider.System);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 // Configure the HTTP request pipeline.
