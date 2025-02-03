b3b15b6eac1deeb293055d532f7965b9e3f8b56c
meewaldor <tieumylam91@gmail.com>
2025-02-03 17:52:28 +0700

    Add SubjectsController

---
+++ TeamoWeb.API/Controllers/SubjectsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Teamo.Core.Entities;
+using Teamo.Core.Interfaces;
+
+namespace TeamoWeb.API.Controllers
+{
+    public class SubjectsController : BaseApiController
+    {
+        private readonly IGenericRepository<Subject> _subjectsRepository;
+        public SubjectsController(IGenericRepository<Subject> subjectsRepository)
+        {
+            _subjectsRepository = subjectsRepository;
+        }
+        public async Task<ActionResult<Subject>> GetSubjectsByMajorId(int id)
+        {
+            
+        }
+    }
+}
