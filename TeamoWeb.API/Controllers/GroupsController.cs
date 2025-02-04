14b5c681c89eee842fc71405adf01c54fad730a2
meewaldor <tieumylam91@gmail.com>
2025-02-04 11:00:25 +0700

    My - Add GroupController Endpoint #16

---
+++ TeamoWeb.API/Controllers/GroupsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Teamo.Core.Entities;
+using Teamo.Core.Interfaces.Services;
+using Teamo.Core.Specifications;
+using Teamo.Core.Specifications.Groups;
+using TeamoWeb.API.Dtos;
+using TeamoWeb.API.Extensions;
+using TeamoWeb.API.RequestHelpers;
+
+namespace TeamoWeb.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : BaseApiController
+    {
+        private readonly IGroupService _groupService;
+        public GroupsController(IGroupService groupService)
+        {
+            _groupService = groupService;
+        }
+        
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<IReadOnlyList<GroupDto>>> GetGroupsAsync([FromQuery] GroupParams groupParams)
+        {
+            var spec = new GroupSpecification(groupParams);
+            var groups = await _groupService.GetGroupsAsync(spec) ?? new List<Group>();
+            var groupDtos = groups.Any() ? groups.Select(g => g.ToDto()).ToList() : new List<GroupDto?>();
+            var pagination = new Pagination<GroupDto>(groupParams.PageIndex, groupParams.PageSize, groups.Count(), groupDtos);
+            return Ok(pagination);
+        }
+    }
+}
