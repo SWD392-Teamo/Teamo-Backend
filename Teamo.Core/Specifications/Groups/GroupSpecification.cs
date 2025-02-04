206a3f89b66f6df67721ac4640c19cc397f1d5d9
meewaldor <tieumylam91@gmail.com>
2025-02-04 10:09:56 +0700

    My - Add GroupSpecification for searching, filtering and paging #16

---
+++ Teamo.Core/Specifications/Groups/GroupSpecification.cs
@@ -0,0 +1,28 @@
+﻿
+using Teamo.Core.Entities;
+
+namespace Teamo.Core.Specifications.Groups
+{
+    public class GroupSpecification : BaseSpecification<Group>
+    {
+        public GroupSpecification(GroupParams groupParams)
+            : base(x => (string.IsNullOrEmpty(groupParams.Search)
+            || x.GroupPositions.Any(gp => gp.Name.ToLower().Contains(groupParams.Search))) &&
+            (!groupParams.SubjectId.HasValue || x.SubjectId == groupParams.SubjectId)
+            )
+        {
+            AddInclude(x => x.GroupPositions);
+            AddInclude(x => x.GroupMembers);
+            AddInclude(x => x.Applications);
+            ApplyPaging(groupParams.PageSize * (groupParams.PageIndex - 1),
+                groupParams.PageSize);
+        }
+        public GroupSpecification(int id)
+            : base(x => x.Id == id)
+        {
+            AddInclude(x => x.GroupPositions);
+            AddInclude(x => x.GroupMembers);
+            AddInclude(x => x.Applications);
+        }
+    }
+}
