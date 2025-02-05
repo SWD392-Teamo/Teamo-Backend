f3a32faab40f172d9bdbe286c8b2a2c65f3303da
meewaldor <tieumylam91@gmail.com>
2025-02-05 22:50:56 +0700

    My - Seed data for Group, GroupMember, GroupPosition #25

--- Teamo.Core/Enums/GroupStatus.cs
+++ Teamo.Core/Enums/GroupStatus.cs
@@ -1,9 +1,16 @@
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
 namespace Teamo.Core.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum GroupStatus
     {
+        [EnumMember(Value = "Recruiting")]
         Recruiting,
+        [EnumMember(Value = "Full")]
         Full,
+        [EnumMember(Value = "Archived")]
         Archived
     }
 }
