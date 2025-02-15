446568904e3d21b645cf1f132a7191cc7bd2d2c6
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-02-16 00:04:17 +0700

    Add Semester specification #60

---
+++ Teamo.Core/Specifications/Semesters/SemesterParams.cs
@@ -0,0 +1,15 @@
+﻿using Teamo.Core.Enums;
+
+namespace Teamo.Core.Specifications.Semesters
+{
+    public class SemesterParams
+    {
+        private string _search;
+        public string Search
+        {
+            get => _search;
+            set => _search = value.ToLower();
+        }
+        public SemesterStatus? Status { get; set; }
+    }
+}
---
+++ Teamo.Core/Specifications/Semesters/SemesterSpecification.cs
@@ -0,0 +1,14 @@
+﻿using Teamo.Core.Entities;
+
+namespace Teamo.Core.Specifications.Semesters
+{
+    public class SemesterSpecification : BaseSpecification<Semester>
+    {
+        public SemesterSpecification(SemesterParams semesterParams)
+            :base(x => (!string.IsNullOrEmpty(semesterParams.Search) || semesterParams.Search.ToLower() == x.Name.ToLower() 
+            || semesterParams.Search.ToLower() == x.Code.ToLower()) 
+            && (!semesterParams.Status.HasValue || semesterParams.Status == x.Status))
+        {
+        }
+    }
+}
