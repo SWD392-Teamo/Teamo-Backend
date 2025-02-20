bdc7de1dd589c90e598dcb37e46b58525b49f368
khanhcnp <khanhcnpse181509@fpt.edu.vn>
2025-02-20 14:06:30 +0700

    Changes to ApplicationsController and GroupsController

--- TeamoWeb.API/Controllers/GroupsController.cs
+++ TeamoWeb.API/Controllers/GroupsController.cs
@@ -5,8 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
 using System.Text.RegularExpressions;
 using Teamo.Core.Entities;
+using Teamo.Core.Enums;
 using Teamo.Core.Interfaces.Services;
 using Teamo.Core.Specifications;
+using Teamo.Core.Specifications.Applications;
 using Teamo.Core.Specifications.Groups;
 using TeamoWeb.API.Dtos;
 using TeamoWeb.API.Errors;
@@ -19,11 +21,13 @@ namespace TeamoWeb.API.Controllers
     public class GroupsController : BaseApiController
     {
         private readonly IGroupService _groupService;
+        private readonly IApplicationService _appService;
         private readonly IUserService _userService;
-        public GroupsController(IGroupService groupService, IUserService userService)
+        public GroupsController(IGroupService groupService, IUserService userService, IApplicationService appService)
         {
             _groupService = groupService;
             _userService = userService;
+            _appService = appService;
         }
 
         /// <summary>
@@ -214,5 +218,99 @@ namespace TeamoWeb.API.Controllers
             groupMember = await _groupService.GetGroupMemberAsync(groupId, studentId);
             return Ok(groupMember.ToDto());
         }
+
+        //Get group's applications with spec
+        [HttpGet("{groupId}/applications")]
+        [Authorize(Roles = "Student")]
+        public async Task<ActionResult<IReadOnlyList<ApplicationDto>>> GetGroupApplications(int groupId, [FromQuery] ApplicationParams appParams)
+        {
+            appParams.GroupId = groupId;
+
+            //Check if current user is the leader of corresponding group
+            var user = await _userService.GetUserByClaims(HttpContext.User);
+            if (user == null)
+                return Unauthorized(new ApiErrorResponse(401, "Unauthorized"));
+
+            var groupLeaderId = await _appService.GetGroupLeaderIdAsync(groupId);
+            if(user.Id != groupLeaderId) 
+                return BadRequest(new ApiErrorResponse(400, "Only group leaders can view applications"));
+
+            var appSpec = new ApplicationGroupSpecification(appParams);
+            var apps = await _appService.GetGroupApplicationsAsync(appSpec);
+            var count = await _appService.CountAsync(appSpec);
+            var appsToDto = apps.Select(a => a.ToDto()).ToList();
+            var pagination = new Pagination<ApplicationDto>(appParams.PageIndex,appParams.PageSize,count,appsToDto);
+            return Ok(pagination);
+        }
+
+        //Approve or reject application
+        [HttpPatch("{groupId}/applications/{appId}")]
+        [Authorize(Roles = "Student")]
+        public async Task<ActionResult> ReviewApplication(int groupId, int appId, [FromBody] ApplicationToUpsertDto appDto)
+        {
+            //Check if current user is the leader of corresponding group
+            var user = await _userService.GetUserByClaims(HttpContext.User);
+            if (user == null)
+                return Unauthorized(new ApiErrorResponse(401, "Unauthorized"));
+
+            var groupLeaderId = await _appService.GetGroupLeaderIdAsync(groupId);
+            if(user.Id != groupLeaderId) 
+                return BadRequest(new ApiErrorResponse(400, "Only group leaders can review applications."));
+            
+            var app = await _appService.GetApplicationByIdAsync(appId);
+            if(app == null || app.Status != ApplicationStatus.Requested) 
+                return BadRequest(new ApiErrorResponse(400, "Unable to review this application."));
+
+            appDto.ToEntity(app);
+
+            var result = await _appService.ReviewApplicationAsync(app);
+            if(!result) return BadRequest(new ApiErrorResponse(400, "Failed to review application."));
+            return Ok(new ApiErrorResponse(200, "Application reviewed successfully."));
+        }
+
+        //Create and send an application
+        [HttpPost("{groupId}/applications")]
+        [Authorize(Roles = "Student")]
+        public async Task<ActionResult<ApplicationDto>> CreateNewApplication(int groupId, [FromBody] ApplicationToUpsertDto appDto)     
+        {            
+            var user = await _userService.GetUserByClaims(HttpContext.User);
+            if (user == null)
+                return Unauthorized(new ApiErrorResponse(401, "Unauthorized"));
+            
+            appDto.StudentId = user.Id;
+            appDto.GroupId = groupId;
+
+            //Check for validity to apply
+            var isValid = await _appService.CheckValidToApply(appDto.GroupId,appDto.StudentId,appDto.GroupPositionId);
+            if(!isValid) return BadRequest(new ApiErrorResponse(400, "Not applicable to create application."));
+
+            var app = appDto.ToEntity();
+            
+            var result = await _appService.CreateNewApplicationAsync(app);
+            if(!result) return BadRequest(new ApiErrorResponse(400, "Failed to create and send application."));
+            return Ok(new ApiErrorResponse(200, "Application sent successfully."));
+        }
+
+        //Delete application (recall unanswered application)
+        [HttpDelete("{groupId}/applications/{appId}")]
+        [Authorize(Roles = "Student")]
+        public async Task<ActionResult> DeleteApplication(int appId)
+        {
+            var app = await _appService.GetApplicationByIdAsync(appId);
+            if(app == null) return NotFound(new ApiErrorResponse(404, "Application not found."));
+
+            //Check if current user is the sender of the corresponding application
+            var user = await _userService.GetUserByClaims(HttpContext.User);
+            if (user == null || user.Id != app.StudentId)
+                return Unauthorized(new ApiErrorResponse(401, "Unauthorized"));
+            
+            if(app.Status != ApplicationStatus.Requested)
+                return BadRequest(new ApiErrorResponse(400, "The application has been reviewed."));
+
+            var result = await _appService.DeleteApplicationAsync(app);
+            if(!result) return BadRequest(new ApiErrorResponse(400, "Failed to delete application."));
+            return Ok(new ApiErrorResponse(200, "Application deleted successfully."));
+        }
+
     }
 }
