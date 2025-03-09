fbf88885291fec0aac61d7c0f68bd79eb1bf2f66
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-03-09 15:47:25 +0700

    Get Own Groups Endpoint

--- TeamoWeb.API/Controllers/GroupsController.cs
+++ TeamoWeb.API/Controllers/GroupsController.cs
@@ -52,6 +52,26 @@ namespace TeamoWeb.API.Controllers
             var pagination = new Pagination<GroupDto>(groupParams.PageIndex, groupParams.PageSize, groups.Count(), groupDtos);
             return Ok(pagination);
         }
+        /// <summary>
+        /// Retrieves a list of user's groups with pagination.
+        /// </summary>
+        [Cache(1000)]
+        [HttpGet("me")]
+        [Authorize]
+        public async Task<ActionResult<IReadOnlyList<GroupDto>>> GetOwnGroupsAsync([FromQuery] GroupParams groupParams)
+        {
+            var user = await _userService.GetUserByClaims(HttpContext.User);
+            if (user == null)
+                return Unauthorized(new ApiErrorResponse(401, "User not authenticated."));
+
+            groupParams.StudentId = user.Id;    
+            var spec = new GroupSpecification(groupParams);
+            var groups = await _groupService.GetGroupsAsync(spec) ?? new List<Group>();
+
+            var groupDtos = groups.Any() ? groups.Select(g => g.ToDto()).ToList() : new List<GroupDto?>();
+            var pagination = new Pagination<GroupDto>(groupParams.PageIndex, groupParams.PageSize, groups.Count(), groupDtos);
+            return Ok(pagination);
+        }
 
         /// <summary>
         /// Retrieves group details by ID.
