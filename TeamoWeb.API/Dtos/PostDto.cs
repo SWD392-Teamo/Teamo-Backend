5e14b3b2c014f26b346d8f4d02e26b61b1f14993
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-03-19 22:43:44 +0700

    update create and update post endpoints to accept form-data

--- TeamoWeb.API/Dtos/PostDto.cs
+++ TeamoWeb.API/Dtos/PostDto.cs
@@ -13,8 +13,9 @@ namespace TeamoWeb.API.Dtos
         public string Content { get; set; } = string.Empty;
         public PostStatus Status { get; set; }
         public PostPrivacy Privacy { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } 
         public DateTime? UpdatedAt { get; set; }
+        public string? DocumentUrl { get; set; }
     }
 }
 
